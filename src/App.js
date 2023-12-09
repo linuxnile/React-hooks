01c6ce38b90f99ef74793d5cdcfc94f912a6c326
Linux Nile <linuxnile@gmail.com>
2023-12-09 09:46:58 +0530

    useReducer completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-// import UseState from "./component/Hooks/useState";
-import UseEffect from "./component/Hooks/useEffect";
+import UseReducer from "./component/Hooks/useReducer";
 
 const App = () => {
-  return <UseEffect />;
+  return <UseReducer />;
 };
 
 export default App;
